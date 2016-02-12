c741a1d1de2a7b817e0e0543d686d3be2538412d
jpiiro <jarkkop+jpiiro@gmail.com>
2016-02-12 13:55:59 +0200

    TecDoc-asetukset
    Provider on korvattava oikealla koodilla.

---
+++ tecdoc_asetukset.php
@@ -0,0 +1,7 @@
+<?php
+
+define('TECDOC_PROVIDER', 0);  // Korvaa tämä oikealla koodilla!
+define('TECDOC_SERVICE_URL', 'http://webservicepilot.tecdoc.net/pegasus-3-0/services/TecdocToCatDLB.jsonEndpoint');
+define('TEDDOC_DEBUG', false);
+define('TECDOC_COUNTRY', 'FI');
+define('TECDOC_LANGUAGE', 'FI');
