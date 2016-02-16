2c91a2a8ba42ecbe8ce93617f698aa73e4c92845
jpiiro <jarkkop+jpiiro@gmail.com>
2016-02-16 20:15:34 +0200

    Oma tiedosto tietokannan asetuksille
    Käytetään mieluummin vaikka tätä, jottei joka tiedostossa tarvitse
    kirjoitella tietokannan tietoja erikseen.

---
+++ tietokanta.php
@@ -0,0 +1,10 @@
+<?php
+
+//
+// Tietokannan asetukset
+//
+
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME', 'tuoteluettelo_database');
