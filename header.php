50f6c21d0b2fbb38ed9347d428579d2e2d475016
jpiiro <jarkkop+jpiiro@gmail.com>
2016-02-22 19:05:51 +0200

    Apufunktioita kirjautumistarkistuksiin.
    Käyttäjä ohjautuu nyt login-sivulle jos ei ole kirjautunut sisään.

--- header.php
+++ header.php
@@ -1,11 +1,34 @@
+<?php
+
+session_start();
+
+//
+// Kirjautumiseen liittyviä apufunktioita
+//
+
+function is_logged_in() {
+	return isset($_SESSION['email']);
+}
+
+function is_admin() {
+	return isset($_SESSION['admin']) && $_SESSION['admin'] == 1;
+}
+
+// Tarkistetaan, onko käyttäjä kirjautunut sisään;
+// jos ei, niin ohjataan suoraan kirjautumissivulle.
+if (!is_logged_in()) {
+    header('Location: login.php');
+    exit;
+}
+
+?>
 <div id="header">
 	<img alt="Logo" src="img/rantak_varao-Logo.jpg" height="100" width="300" align="left">
 	<!-- <h1>Logo</h1> -->
 	<p class="kirjautuneena">Kirjautuneena:</p>
 	<?php
-		session_start();
 		//tarkastaa onko nimi talletettu sessioniin
-		if(isset($_SESSION['email'])){
+		if (is_logged_in()){
 			echo ('<p class="username">' . $_SESSION['email'] . '</p>');
 		}
 	?>
@@ -16,7 +39,7 @@
         <li><a href='tuotehaku.php'><span>Tuotehaku</span></a></li>
         <li><a href='omat_tiedot.php'><span>Omat tiedot</span></a></li>
     <?php
-        if (isset($_SESSION['admin']) && $_SESSION['admin'] == 1) {
+        if (is_admin()) {
     ?>
         <li><a href='yp_asiakkaat.php'><span>Asiakkaat</span></a></li>
         <li><a href='yp_hinnat.php'><span>Hinnat</span></a></li>
@@ -29,7 +52,11 @@
         <li><a href='tilaushistoria.php'><span>Tilaushistoria</span></a></li>
     <?php
         }
+		if (is_logged_in()) {
+			echo '<li class="last"><a href="logout.php"><span>Kirjaudu ulos</span></a></li>';
+		} else {
+			echo '<li class="last"><a href="login.php"><span>Kirjaudu sisään</span></a></li>';
+		}
     ?>
-        <li class="last"><a href='logout.php'><span>Kirjaudu ulos</span></a></li>
 	</ul>
 </div>
