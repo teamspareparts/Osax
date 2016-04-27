945b3fa7c49b343c8af779888bf775cf4ec11156
jpiiro <jarkkop+jpiiro@gmail.com>
2016-04-27 19:42:58 +0300

    Skripti asennuksen helpottamiseksi
    Asiakkaalle ehkä helpompaa jos ei tarvitse lisäillä ylläpitäjää käsin
    tietokantaan ym.

---
+++ tietokanta/asenna.php
@@ -0,0 +1,47 @@
+<?php
+
+//
+// Tämä tiedosto alustaa tietokannan siten, että järjestelmä on sen jälkeen käyttövalmis.
+// Oletuksena tietokantaan luodaan uusi ylläpitäjä, jonka käyttäjätunnus ja salasana on admin.
+// Voit halutessasi muokata käyttäjätunnusta ja salasanaa alla olevilla muuttujilla.
+//
+
+$yllapitajan_tunnus = 'admin@example.com';
+$yllapitajan_salasana = 'admin';
+
+//
+// Älä muokkaa tästä eteenpäin!
+//
+
+require '../tietokanta.php';
+
+$yhteys = @mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME) or die('Tietokantayhteyttä ei voitu muodostaa.<br>Virhe: ' . mysqli_connect_error() . '<br>Tarkista tietokannan asetukset tiedostossa <i>tietokanta.php</i>!');
+
+// Ei tehdä mitään, jos tietokanta on jo alustettu
+if (mysqli_query($yhteys, 'SELECT 1 FROM kayttaja LIMIT 1;')) {
+    die('Tietokanta on jo alustettu!');
+}
+
+// Luodaan tietokannan taulut
+if (mysqli_multi_query($yhteys, file_get_contents('tietokanta.sql'))) {
+    echo 'Tietokanta alustettiin onnistuneesti.<br>';
+} else {
+    echo 'Jokin meni pieleen tietokantaa alustettaessa<br>Virhe: ' . mysqli_error($yhteys) . '<br>Tarkista tietokanta ja suorita tarvittaessa <i>tietokanta.sql</i> käsin.';
+}
+
+// Luodaan ylläpitäjälle käyttäjä
+$yllapitajan_tunnus = addslashes($yllapitajan_tunnus);
+$hajautettu_salasana = password_hash($yllapitajan_salasana, PASSWORD_DEFAULT);
+$kysely = "INSERT INTO kayttaja (sahkoposti, salasana_hajautus, yllapitaja) VALUES ('$yllapitajan_tunnus', '$hajautettu_salasana', 1);";
+
+while (mysqli_more_results($yhteys)) {
+    mysqli_next_result($yhteys);
+    if ($tulos = mysqli_store_result($yhteys))
+        $tulos->free();
+}
+
+if (!mysqli_query($yhteys, $kysely)) {
+    echo 'Ylläpitäjä luotu.';
+} else {
+    echo 'Ylläpitäjän luonti ei onnistunut!<br>Virhe: ' . mysqli_error($yhteys);
+}
