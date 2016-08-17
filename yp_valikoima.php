3cee46945b56a2f18c604e5c0c059164715b706c
juhanj <juhani.jarvio@gmail.com>
2016-08-17 14:46:30 +0300

    Yp_Valikoima toiminnallisesti aika valmis
    Tuotteiden tulostust testattu. 5000 tuotteen tulostuksessa yhdelle
    sivulle se saattaa ladata hetken, ainakin lokaalisti, mutta toimii
    silti. Ei testattu sen pitemmälle.
    
    Valikoiman sivustus toimii... joten kuten. Käyttäjän valinta
    n-sivuvaihdolle, ja tuotteet-per-sivu -valinnalle ei toimi vielä. Niitä
    voi kuitenkin käyttää URL:issa GET:in avulla.
    URL?value=value&...
    - brand=[number] (default on "all", jos et pistä mitään URL:iin)
    - page=[number] (Ei voi olla suurempi kuin total_pages. Kinda totally
    breaks otherwise.)
    - ppp=[number] (Voi olla mikä tahansa numero, ei rajoitusta. Yli 5000 ei
    testattu.)
    
    Actually, muistaakseen Osax:illa oli jotain ongelmaa tuon
    fetchAll-metodin kanssa. Joten siellä ei saata edes päästä sinne 5000
    asti.
    
    Eli alunperin tuossa oli brändi defaultina 206 (Gates). Mutta sitten
    ajattelin, että miksei tulosteta vaan koko tietokantaa kun kerran
    pystyy?
    I give you: koko valikoiman tulostus. Eipähän tarvinnu lähteä
    tiedostonimeä muuttamaan pitemmäksi.

--- yp_valikoima.php
+++ yp_valikoima.php
@@ -7,80 +7,158 @@
 	<title>Valikoima</title>
 	<link rel="stylesheet" href="css/styles.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/css/bootstrap.min.css" integrity="sha384-MIwDKRSSImVFAZCVLtU0LMDdON6KVCrZHyVQQj6e8wIEJkW4tvwqXrbMIya1vriY" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<style>
+		.material-icons {
+			display: flex;
+			margin: auto;
+		}
+		.page_nav {
+			display: flex;
+			flex-direction: row;
+		}
+		.page_control {
+			display: flex;
+			flex-direction: column;
+			margin: auto 40px auto;
+		}
+		.pageNumberForm, .pageNumber {
+			display: inline-flex;
+			padding: 0;
+			margin: 0;
+			border: 0;
+			width: 2.7rem;
+		}
+	</style>
 </head>
 <body>
 <?php
 require 'header.php';
 require 'tietokanta.php';
 
+/**
+ * @param DByhteys $db <p> Tietokanta-yhteys
+ * @param int|string $brandNo <p> Minkä brändin tuotteet haetaan.
+ * 		Jos == "all", niin tulostaa kaikki tietokannassa olevat tuotteet.
+ * @param int $ppp <p> Montako tuotetta kerralla ruudussa.
+ * @param int $offset <p> Mistä tuotteesta aloitetaan palautus.
+ * @return array <p> Arrayn tuloksista objekteina
+ */
+function haeTuotteet ($db, $brandNo, $ppp, $offset) {
+	if ( $brandNo !== "all") {
+		$query = "	SELECT *, 
+						( SELECT COUNT(id) FROM tuote WHERE brandNo = ? ) AS row_count
+					FROM tuote 
+					WHERE brandNo = ?
+					LIMIT {$ppp} OFFSET {$offset}";
+
+		$result = $db->query( $query, [$brandNo, $brandNo], FETCH_ALL, PDO::FETCH_OBJ );
+	} else {
+		$query = "	SELECT *,
+						( SELECT COUNT(id) FROM tuote ) AS row_count
+					FROM tuote
+					LIMIT {$ppp} OFFSET {$offset}";
+
+		$result = $db->query( $query, NULL, FETCH_ALL, PDO::FETCH_OBJ );
+	}
+
+	return $result;
+}
 
 if ( !is_admin() ) { header("Location:etusivu.php"); exit(); }
 //if ( !isset($_GET['brand']) ) { header("Location:yp_toimittajat.php"); exit(); }
-$brand = isset($_GET['brand']) ? (int)$_GET['brand'] : 1; //TODO: Temp solution
+$brand = isset($_GET['brand']) ? $_GET['brand'] : "all"; //TODO: Temp solution
 $page = isset($_GET['page']) ? (int)$_GET['page'] : 1; // Mikä sivu tuotelistauksessa
-$products_per_page = 20; // Miten monta tuotetta per sivu näytetään. Ei aikomusta olla käyttäjän muutettavissa.
+$products_per_page = isset($_GET['ppp']) ? (int)$_GET['ppp'] : 20; // Miten monta tuotetta per sivu näytetään.
+if ( $page < 1 ) { $page = 1; }
+if ( $products_per_page < 1 ) { $products_per_page = 1; }
 $offset = ($page-1) * $products_per_page; // SQL-lausetta varten; kertoo monennestako tuloksesta aloitetaan haku
 
-$query = "	SELECT *, COUNT(id) as row_count
-			FROM tuote 
-			WHERE brandNo = ?
-			LIMIT {$products_per_page} OFFSET {$offset}";
+$products = haeTuotteet($db, $brand, $products_per_page, $offset);
 
-//echo "<pre>";
-//echo $query;
-$products = $db->query( $query, [$brand], FETCH_ALL, PDO::FETCH_OBJ );
+$total_products = isset($products[0]->row_count) ? $products[0]->row_count : 0;
+if ( $total_products < $products_per_page ) { $products_per_page = $total_products; }
+if ( $total_products !== 0 ) { $total_pages = ceil($total_products / $products_per_page); }
+else { $total_pages = 1; }
 ?>
 
 <main>
-	Pagination not implemented yet
-	<nav aria-label="Page navigation">
+	<nav aria-label="Page navigation" class="page_nav">
 		<ul class="pagination">
-			<li class="page-item disabled">
-				<a class="page-link" href="?brand=<?=$brand?>" aria-label="Previous">
-					<span aria-hidden="true">&LeftArrowBar;</span>
-					<span class="sr-only">Previous</span>
+			<li class="page-item backward_nav" id="first_page">
+				<a class="page-link" href="?brand=<?=$brand?>">
+					<i class="material-icons">first_page</i>
+					First
 				</a>
 			</li>
-			<li class="page-item disabled">
+			<li class="page-item backward_nav" id="previous_page">
 				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$page-1?>" aria-label="Previous">
-					<span aria-hidden="true">&laquo;</span>
-					<span class="sr-only">Previous</span>
+					<i class="material-icons">arrow_back</i>
+					Previous
 				</a>
 			</li>
 
 			<li class="page-item active"><span class="page-link">
-					[DisplayedItemRange] of [TotalItems]</span></li>
+					Sivu:
+					<form class="pageNumberForm" type="GET">
+						<input class="hidden" name="brand" value="<?=$brand?>"/>
+						<input type="number" name="page" class="pageNumber"
+							   min="1" max="<?=$total_pages?>" value="<?=$page?>" maxlength="3"/>
+						<?=isset($_GET['ppp'])? "<input class='hidden' name='brand' value='{$_GET['ppp']}'/>" :NULL?>
+					</form>​
+					 / <?=$total_pages?><br>
+					Tuotteet: <?=$offset?>&ndash;<?=$offset + $products_per_page?> / <?=$total_products?></span></li>
 
-			<li class="page-item">
+			<li class="page-item forward_nav" id="next_page">
 				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$page+1?>" aria-label="Next">
-					<span aria-hidden="true">&raquo;</span>
-					<span class="sr-only">Next</span>
+					<i class="material-icons">arrow_forward</i>
+					Next
 				</a>
 			</li>
-			<li class="page-item">
-				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$page?>" aria-label="Next">
-					<span aria-hidden="true">&RightArrowBar;</span>
-					<span class="sr-only">Next</span>
+			<li class="page-item forward_nav" id="last_page">
+				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$total_pages?>">
+					<i class="material-icons">last_page</i>
+					Last
 				</a>
 			</li>
 		</ul>
+		<div class="page_control">
+			<span>Valitse sivunumero, ja paina Enter-näppäintä vaihtaaksesi sivua. (ei toimi tällä hetkellä kunnolla.)</span>
+			<span>Tuotteita per sivu:
+				<form class="pageNumberForm" type="GET">
+					<select>
+						<option value="10">10</option>
+						<option value="20" selected>20</option>
+						<option value="30">30</option>
+						<option value="50">50</option>
+						<option value="100">100</option>
+						<option value="500">500</option>
+						<option value="1000">1000</option>
+						<option value="5000">5000</option>
+						<option value="10000">10000</option>
+					</select>
+				</form>
+			</span> (Tulossa myöhemmin. Jos haluat kokeilla nyt, lisää URL:iin "&ppp=[some number]")<br>
+			Huom. Toimii tällä hetkellä ainakin 5000 tuotteesen asti, mutta sen jälkeen saattaa esiintyä joitain outouksia.
+		</div>
 	</nav>
 
+	<p>Taulukon viimeistely tulee myöhemmin</p>
 	<table>
 		<thead>
 		<tr><td>ID</td>
 			<td>ArtNo</td>
 			<td>BrandNo</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td>
-			<td>#</td></tr>
+			<td>Hinta</td>
+			<td>ALV</td>
+			<td>Saldo</td>
+			<td>Min.erä</td>
+			<td>Sis.ostohinta</td>
+			<td>Yht.kpl</td>
+			<td>Keskios.hinta</td>
+			<td>Alen.kpl</td>
+			<td>Alen.%</td>
+			<td>Akt.</td></tr>
 		</thead>
 
 		<tbody>
@@ -101,10 +179,95 @@ $products = $db->query( $query, [$brand], FETCH_ALL, PDO::FETCH_OBJ );
 		<?php endforeach; ?>
 		</tbody>
 	</table>
-</main>
 
+	<nav aria-label="Page navigation" class="page_nav">
+		<ul class="pagination">
+			<li class="page-item backward_nav" id="first_page">
+				<a class="page-link" href="?brand=<?=$brand?>">
+					<i class="material-icons">first_page</i>
+					First
+				</a>
+			</li>
+			<li class="page-item backward_nav" id="previous_page">
+				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$page-1?>" aria-label="Previous">
+					<i class="material-icons">arrow_back</i>
+					Previous
+				</a>
+			</li>
+
+			<li class="page-item active"><span class="page-link">
+					Sivu:
+					<form class="pageNumberForm" type="GET">
+						<input class="hidden" value="<?=$brand?>"/>
+						<input type="number" name="page" class="pageNumber"
+							   min="1" max="<?=$total_pages?>" value="<?=$page?>" maxlength="3"/>
+					</form>​
+					 / <?=$total_pages?><br>
+					Tuotteet: <?=$offset?>&ndash;<?=$offset + $products_per_page?> / <?=$total_products?></span></li>
+
+			<li class="page-item forward_nav" id="next_page">
+				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$page+1?>" aria-label="Next">
+					<i class="material-icons">arrow_forward</i>
+					Next
+				</a>
+			</li>
+			<li class="page-item forward_nav" id="last_page">
+				<a class="page-link" href="?brand=<?=$brand?>&page=<?=$total_pages?>">
+					<i class="material-icons">last_page</i>
+					Last
+				</a>
+			</li>
+		</ul>
+		<div class="page_control">
+			<span>Valitse sivunumero, ja paina Enter-näppäintä vaihtaaksesi sivua.</span>
+			<span>Tuotteita per sivu:
+				<form class="pageNumberForm" type="GET">
+					<select>
+						<option value="10">10</option>
+						<option value="20" selected>20</option>
+						<option value="30">30</option>
+						<option value="50">50</option>
+						<option value="100">100</option>
+						<option value="500">500</option>
+						<option value="1000">1000</option>
+						<option value="5000">5000</option>
+						<option value="10000">10000</option>
+					</select>
+				</form>
+			</span> (Tulossa myöhemmin. Jos haluat kokeilla nyt, lisää URL:iin "&ppp=[some number]")<br>
+			Huom. Toimii tällä hetkellä ainakin 5000 tuotteesen asti, mutta sen jälkeen saattaa esiintyä joitain outouksia.
+		</div>
+	</nav>
+
+</main>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.3/js/bootstrap.min.js" integrity="sha384-ux8v3A6CPtOTqOzMKiuo3d/DomGaaClxFYdCu2HPMBEkf6x2xiDyJ7gkXU0MWwaD" crossorigin="anonymous"></script>
+<!--suppress JSUnusedAssignment -->
+<script>
+	var backwards = document.getElementsByClassName('backward_nav');
+	var forwards = document.getElementsByClassName('forward_nav');
+	var total_pages = <?= $total_pages?>;
+	var current_page = <?= $page?>;
+	var i = 0; //for-looppia varten
+
+	if ( current_page === 1 ) { //Tarkistetaan taaksepäin-nappien käytettävyys
+		for ( i=0; i< backwards.length; i++ ) {
+			backwards[i].className += " disabled";
+		}
+	}
+	if ( current_page === total_pages ) { // Tarkistetaan eteenpäin-nappien käytettävyys
+		for ( i=0; i< forwards.length; i++ ) {
+			forwards[i].className += " disabled";
+		}
+	}
+
+	$('.pageNumberForm').keypress(function (e) {
+		if (e.which == 13) {
+			$('form#pageNumberForm').submit();
+			return false;    //<---- Add this line
+		}
+	});
+</script>
 </body>
 </html>
