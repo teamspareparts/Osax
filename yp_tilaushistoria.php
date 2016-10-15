c7adda011f57234a563d745b990585e0c7506078
juhanj <juhani.jarvio@gmail.com>
2016-10-15 10:38:21 +0300

    Yp_tilaushistoria
    Otettu käyttöön _start. HTML vielä uudistamatta. I'll do it later.
    Pitäisi luoda pari testitilausta, jotta näkisin miten tämä sivu toimii
    käytännössä.

--- yp_tilaushistoria.php
+++ yp_tilaushistoria.php
@@ -1,45 +1,41 @@
+<?php
+require '_start.php'; global $db, $user, $cart, $yritys;
+require 'apufunktiot.php';
+if( !$user->isAdmin() ) {
+	header("Location:etusivu.php"); exit();
+}
+$sql = "SELECT tilaus.id, tilaus.paivamaara, tilaus.kasitelty, kayttaja.etunimi, kayttaja.sukunimi, 
+			SUM( tilaus_tuote.kpl * (tilaus_tuote.pysyva_hinta*(1+tilaus_tuote.pysyva_alv))) AS summa 
+		FROM tilaus 
+		LEFT JOIN kayttaja ON kayttaja.id=tilaus.kayttaja_id
+		LEFT JOIN tilaus_tuote ON tilaus_tuote.tilaus_id=tilaus.id
+		GROUP BY tilaus.id
+		ORDER BY tilaus.id DESC";
+$tilaukset = $db->query( $query, NULL, FETCH_ALL );
+?>
 <!DOCTYPE html>
 <html lang="fi">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/styles.css">
 	<title>Tilaukset</title>
+	<link rel="stylesheet" href="css/styles.css">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 </head>
 <body>
-<?php 	
-include 'header.php';
-?>
-<h1 class="otsikko">Tilaushistoria</h1>
-<br><br>
+<?php include 'header.php'; ?>
+<main class="main_body_container">
 
+</main>
 <div id="tilaukset">
 	<div id="lista">
 
 		<form>
-		<fieldset class="lista_info">
-			<p><span class="tilausnumero">Tilausnro.</span><span class="pvm">Päivämäärä</span><span class="tilaaja">Tilaaja</span><span class="sum">Summa</span>Käsitelty</p>
-		</fieldset>
+			<fieldset class="lista_info">
+				<p><span class="tilausnumero">Tilausnro.</span><span class="pvm">Päivämäärä</span><span class="tilaaja">Tilaaja</span><span class="sum">Summa</span>Käsitelty</p>
+			</fieldset>
 
 			<?php
-			require 'tietokanta.php';
-			require 'apufunktiot.php';
-			if(!is_admin()){
-				header("Location:etusivu.php");
-				exit();
-			}
-
-			$query = "
-				SELECT tilaus.id, tilaus.paivamaara, tilaus.kasitelty, kayttaja.etunimi, kayttaja.sukunimi, 
-					SUM( tilaus_tuote.kpl * (tilaus_tuote.pysyva_hinta*(1+tilaus_tuote.pysyva_alv))) AS summa 
-				FROM tilaus 
-				LEFT JOIN kayttaja 
-					ON kayttaja.id=tilaus.kayttaja_id
-				LEFT JOIN tilaus_tuote
-					ON tilaus_tuote.tilaus_id=tilaus.id
-				GROUP BY tilaus.id
-				ORDER BY tilaus.id DESC";
-			$tilaukset = $db->query($query, [], FETCH_ALL, PDO::FETCH_OBJ);
-
 			foreach ($tilaukset as $tilaus){
 				echo '<fieldset>';
 				echo '<a href="tilaus_info.php?id=' . $tilaus->id . '"><span class="tilausnumero">' . $tilaus->id .
@@ -54,8 +50,6 @@ include 'header.php';
 				}
 				echo '</a></fieldset>';
 			}
-			
-			mysqli_close($connection);
 			?>
 		</form>
 	</div>
