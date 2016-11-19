6d5af885f212d4cf1a3164bc3ac2f443c85f876a
juhanj <juhani.jarvio@gmail.com>
2016-11-19 15:59:24 +0200

    Header-muutoksia
    Lisäsin Home-ikonin (linkki etusivulle).
    Siirsin toimittajat- ja omat_tiedot-sivut ylläpitäjällä
    dropdown-valikkoon.

--- header.php
+++ header.php
@@ -25,12 +25,11 @@
 
     <section id="navigationbar">
         <ul>
+            <li><a href='etusivu.php'><span style="padding: 15px 20px;"><i class="material-icons">home</i></span></a></li>
             <li><a href='tuotehaku.php'><span>Tuotehaku</span></a></li>
-            <li><a href='omat_tiedot.php'><span>Omat tiedot</span></a></li>
-			<?php if ( $user->isAdmin() ) : ?>
+            <?php if ( $user->isAdmin() ) : ?>
                 <li><a href='yp_yritykset.php'><span>Yritykset</span></a></li>
                 <li><a href='yp_tuotteet.php'><span>Tuotteet</span></a></li>
-                <li><a href='toimittajat.php'><span>Toimittajat</span></a></li>
                 <li><a href='yp_tilaukset.php'><span>Tilaukset</span></a></li>
 
                 <li class="dropdown"><span>Muut<i id="dropdown_icon" class="material-icons">arrow_drop_down</i></span>
@@ -40,10 +39,12 @@
                         <li><a href="yp_hankintapyynnot.php"><span>Hankintapyynnöt</span></a></li>
                         <li><a href="yp_muokkaa_alv.php"><span>ALV-muokkaus</span></a></li>
                         <li><a href="yp_luo_hinnastotiedosto.php"><span>Lataa hinnastot</span></a></li>
+                        <li><a href='toimittajat.php'><span>Toimittajat</span></a></li>
+                        <li><a href='omat_tiedot.php'><span>Omat tiedot</span></a></li>
                     </ul>
                 </li>
-
 			<?php else : ?>
+                <li><a href='omat_tiedot.php'><span>Omat tiedot</span></a></li>
                 <li><a href='tilaushistoria.php'><span>Tilaushistoria</span></a></li>
 			<?php endif; ?>
             <li class="last"><a href="logout.php?redir=5"><span>Kirjaudu ulos</span></a></li>
