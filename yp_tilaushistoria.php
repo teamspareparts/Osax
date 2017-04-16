ac0f5f1d798afd3045722d9b21584a4bd747df09
samileh <smai_lehtinen@hotmail.com>
2017-04-16 13:47:00 +0300

    Tilaus-sivujen summien korjaus
    Lopuillakin tilaussivuilla lasketaan summaan mukaan alennukset ja
    rahtimaksu.
    
    Myyntiraportin korjaus.

--- yp_tilaushistoria.php
+++ yp_tilaushistoria.php
@@ -4,8 +4,11 @@ require 'apufunktiot.php';
 if( !$user->isAdmin() ) {
 	header("Location:etusivu.php"); exit();
 }
-$sql = "SELECT tilaus.id, tilaus.paivamaara, tilaus.kasitelty, kayttaja.etunimi, kayttaja.sukunimi, 
-			SUM( tilaus_tuote.kpl * (tilaus_tuote.pysyva_hinta*(1+tilaus_tuote.pysyva_alv))) AS summa,
+$sql = "SELECT tilaus.id, tilaus.paivamaara, tilaus.kasitelty, tilaus.pysyva_rahtimaksu,
+            kayttaja.etunimi, kayttaja.sukunimi, 
+			SUM( tilaus_tuote.kpl * 
+			    (tilaus_tuote.pysyva_hinta * (1+tilaus_tuote.pysyva_alv) * (1-tilaus_tuote.pysyva_alennus)) )
+			    AS summa,
 			yritys.nimi AS yritys
 		FROM tilaus 
 		LEFT JOIN kayttaja ON kayttaja.id=tilaus.kayttaja_id
@@ -47,7 +50,7 @@ $tilaukset = $db->query( $sql, NULL, FETCH_ALL );
 					<td data-href="tilaus_info.php?id=<?= $tilaus->id ?>"><?= date("d.m.Y", strtotime($tilaus->paivamaara))?></td>
 					<td data-href="tilaus_info.php?id=<?= $tilaus->id ?>"><?= $tilaus->yritys?></td>
 					<td data-href="tilaus_info.php?id=<?= $tilaus->id ?>"><?= $tilaus->etunimi . " " . $tilaus->sukunimi?></td>
-					<td data-href="tilaus_info.php?id=<?= $tilaus->id ?>"><?= format_euros($tilaus->summa)?></td>
+					<td data-href="tilaus_info.php?id=<?= $tilaus->id ?>"><?= format_euros($tilaus->summa + $tilaus->pysyva_rahtimaksu)?></td>
 					<td class="smaller_cell" data-href="tilaus_info.php?id=<?= $tilaus->id ?>">
 						<?php if ( $tilaus->kasitelty === 0 ) : ?>
 							<span style="color: red">EI</span>
