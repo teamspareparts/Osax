8b2e438a715b893794fbf3281b28bc4f17e9db4c
juhanj <juhani.jarvio@gmail.com>
2017-04-25 17:10:14 +0300

    Eula.php siivottu
    Poistin joitain turhia html-tageja, hieman CSS-tiedoston siivousta.
    Nappien värit korjattu joten kuten. Kokonaan virheä tai punainen nappi
    näytti hieman rumalta, joten päädyin pelkän tekstin värittämiseen.
    Lisäksi joitain whitespace muutoksia. Tabeja muutttu välilyönneiksi.

--- eula.php
+++ eula.php
@@ -10,15 +10,15 @@ $db = new DByhteys();
 
 if ( !empty( $_POST[ 'hyvaksy_eula' ] ) ) {
 	$sql = "UPDATE kayttaja SET vahvista_eula = '0' WHERE id = ?";
-	$result = $db->query( $sql, [$_SESSION[ 'id' ]] );
+	$result = $db->query( $sql, [ $_SESSION[ 'id' ] ] );
 
-	if ($result) {
+	if ( $result ) {
 		header( 'Location: etusivu.php' );
 		exit;
 	}
 }
 elseif ( !empty( $_POST[ 'hylkaa_eula' ] ) ) {
-	header( 'Location: index.php?redir=10' ); // 10 == sinun pitää hyväksyä EULA
+	header( 'Location: logout.php?redir=10' ); // 10 == sinun pitää hyväksyä EULA
 	exit;
 }
 
@@ -31,39 +31,34 @@ if ( !$eula_txt ) {
 }
 else {
 	// Ota käyttöön, jos encoding eulassa on ANSI.
-	$eula_txt = mb_convert_encoding($eula_txt, "UTF-8", 'windows-1252');
+	$eula_txt = mb_convert_encoding( $eula_txt, "UTF-8", 'windows-1252' );
 }
+$css_version = filemtime( 'css/login_styles.css' );
 ?>
 <!DOCTYPE html>
 <html lang="fi">
 <head>
 	<meta charset="UTF-8">
-	<link rel="stylesheet" href="css/login_styles.css">
 	<title>Käyttöoikeussopimus</title>
+	<link rel="stylesheet" href="css/login_styles.css?v=<?= $css_version ?>">
 </head>
-<body class="eula">
+<body class="eula_body">
 
-<header id="eula_header">
-	<div id="head_text">
-		<h1>Käyttöoikeussopimus</h1>
-		<h4>Ole hyvä ja hyväksy käyttöoikeussopimus ennen sivuston käyttöä.</h4>
-	</div>
-	<div id="head_logo">
-		<img src="img/osax_logo.jpg">
-	</div>
-</header>
-	
-<main id="eula_body">
+	<header class="eula_header">
+		<div class="head_text">
+			<h1>Käyttöoikeussopimus</h1>
+			<h4>Sinun pitää hyväksyä käyttöoikeussopimus ennen sivuston käyttöä.</h4>
+		</div>
+		<img src="img/osax_logo.jpg" class="head_logo">
+	</header>
 
-	<textarea id="eula_scrollable_textbox" ReadOnly title="Käyttöoikeussopimus">
+	<textarea class="eula_scrollable_textbox" ReadOnly title="Käyttöoikeussopimus">
 		<?php echo $eula_txt; ?>
 	</textarea><br>
 	<form method="post">
-		<input type="submit" name="hyvaksy_eula" value="Hyväksy">
-		<input type="submit" name="hylkaa_eula" value="Hylkää">
+		<input type="submit" name="hyvaksy_eula" value="Hyväksy käyttöoikeussopimus" class="eula_hyvaksy">
+		<input type="submit" name="hylkaa_eula" value="Hylkää, ja palaa kirjautumissivulle" class="eula_hylkaa">
 	</form>
-
-</main>
 
 </body>
 </html>
