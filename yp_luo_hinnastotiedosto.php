66c43cbe140d1a729b8d52e8f7c3407683f8b199
samileh <smai_lehtinen@hotmail.com>
2017-05-27 14:25:02 +0300

    Duplikaattibugi korjattu hinnastossa
    Mikäli hankintapaikalla oli kaksi saman nimistä tuotetta (mutta
    kuitenkin eri brändit), hinnastoon vietiin molemmat tuotteet. Tämä
    aiheuitti sen ongelman, että hinnastossa pystyy olemaan kaksi
    samannimistä tuotetta eikä niitä pysty erottamaan toisistaan. Janne
    halusi, että hinnastoon viedään vain se tuote, kummalla on hyllypaikka.
    Mikäli molemmilla tuotteilla on hyllypaikat tai kumpaakaan ei ole
    hyllyssä, halvempi tuote laitetaan hinnastoon.
    
    dev.osaxilla tuote 4PK800 on hyvä esimerkki.

--- yp_luo_hinnastotiedosto.php
+++ yp_luo_hinnastotiedosto.php
@@ -33,24 +33,43 @@ $hinnastotiedosto = fopen($path.$tiedoston_nimi, "w") or die("Tiedostoa ei voi a
 //fwrite($hinnastotiedosto, chr(0xEF).chr(0xBB).chr(0xBF)); //UTF-8 BOM
 $offset = 0;
 $products = true;
+$edellinen_tuote = null;
 while( $products ) {  //Haetaan fetchCount verran tuotteita kerrallaan
     $products = $db->query("
         SELECT * FROM tuote 
         LEFT JOIN ALV_kanta 
-          ON tuote.ALV_kanta=ALV_kanta.kanta 
-        WHERE aktiivinen=1 AND nimi <> 'UNNAMED' AND nimi IS NOT NULL
+          ON tuote.ALV_kanta = ALV_kanta.kanta 
+        WHERE aktiivinen = 1 AND nimi <> 'UNNAMED' AND nimi IS NOT NULL
+        ORDER BY articleNo
         LIMIT ?, ?",
         [$offset, $fetchCount], FETCH_ALL);
     $offset += $fetchCount;
 
+    if ( !isset($edellinen_tuote) ) { // Alustetaan edellinen_tuote
+        $edellinen_tuote = $products[0];
+    }
+
     //Kirjoitetaan haetut tuotteet tiedostoon
-    foreach ( $products as $p ) {
-    	$p->nimi = isset($p->hyllypaikka) ? ('* ' . $p->nimi) : $p->nimi; // Jos tuotetta hyllyssä, nimen eteen *
-        //Kannassa käytetään UTF-8 encodingia. Muutetaan tuotteen nimi windows-1252 (ANSI) muotoon.
-        $row = str_pad( $p->tuotekoodi, 20 , " " ) .
-                str_pad( number_format((1+$p->prosentti)*$p->hinta_ilman_ALV,2,',',''), 10 , " ", STR_PAD_LEFT ) .
-                str_pad( mb_convert_encoding($p->nimi, "windows-1252", "UTF-8"), 40 , " " ) . "\r\n";
-        fwrite($hinnastotiedosto, $row);
+    foreach ( $products as $tuote ) {
+    	if ( $edellinen_tuote->tuotekoodi != $tuote->tuotekoodi ) {
+		    $edellinen_tuote->nimi = !empty($edellinen_tuote->hyllypaikka) ? ('* ' . $edellinen_tuote->nimi) : $edellinen_tuote->nimi; // Jos tuotetta hyllyssä, nimen eteen *
+		    //Kannassa käytetään UTF-8 encodingia. Muutetaan tuotteen nimi windows-1252 (ANSI) muotoon.
+		    $row = str_pad($edellinen_tuote->tuotekoodi, 20, " ") .
+			    str_pad(number_format((1 + $edellinen_tuote->prosentti) * $edellinen_tuote->hinta_ilman_ALV, 2, ',', ''), 10, " ", STR_PAD_LEFT) .
+			    str_pad(mb_convert_encoding($edellinen_tuote->nimi, "windows-1252", "UTF-8"), 40, " ") . "\r\n";
+		    fwrite($hinnastotiedosto, $row);
+		    $edellinen_tuote = $tuote;
+	    } else {
+    		// Jos duplikaatteja, valitaan se kumpi on hyllyssä
+			if ( empty($edellinen_tuote->hyllypaikka) && !empty($tuote->hyllypaikka) ) {
+				$edellinen_tuote = $tuote;
+			} elseif ( $edellinen_tuote->hyllypaikka == $tuote->hyllypaikka ) {
+				// Jos samat hyllypaikat, priorisoidaan hinnan mukaan
+				if ( ((1 + $edellinen_tuote->prosentti) * $edellinen_tuote->hinta_ilman_ALV) > ((1 + $tuote->prosentti) * $tuote->hinta_ilman_ALV )) {
+					$edellinen_tuote = $tuote;
+				}
+			}
+	    }
     }
 }
 
