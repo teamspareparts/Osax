fa225e0563a1763774a4239db0febf8adb68e147
juhanj <juhani.jarvio@gmail.com>
2017-08-08 11:13:06 +0300

    FTP cronjob viimeistelty
    Tosin vain yhdelle FTP-sivustolle, jos jossain vaiheessa saan toiset
    tunnukset, niin pistän ne erilleen .ini-tiedostoon, ja teen koodista
    hieman siistimmän.

--- cronjob_ftp_tehdassaldot.php
+++ cronjob_ftp_tehdassaldot.php
@@ -5,73 +5,43 @@ set_time_limit(300); // 5min
 require './luokat/dbyhteys.class.php';
 $db = new DByhteys();
 
-
 function debug($var,$var_dump=false){
 	echo"<br>\r\n<pre>Print_r ::<br>\r\n";print_r($var);echo"</pre>";
 	if($var_dump){echo"<br><pre>Var_dump ::<br>\r\n";var_dump($var);echo"</pre><br>\r\n";};
 }
 
-
-//$ftp_server = "FTP.NIPPON-PIECES.COM";
-//$ftp_user_name = "OSAX";
-//$ftp_user_pass = "5G876BBQ=DOJ3J)8+674KQ1(RZRR6KX";
-
-$ftp_server = "www35.zoner.fi";
-$ftp_user_name = "indev@osax.fi";
-$ftp_user_pass = "indev";
-
+$ohita_otsikkorivi = true;
+$rows_in_query_at_one_time = 15000;
+$hankintapaikka_id = 140;
+$ftp_server = "ftp.nippon-pieces.com";
+$ftp_user_name = "osax";
+$ftp_user_pass = "5g876bBq=DOj3J)8+674Kq1(RzrR6kx";
 
 $conn_id = ftp_connect($ftp_server);
-
-// login with username and password
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass);
 
-// check connection
-if ((!$conn_id) || (!$login_result)) {
-	echo "FTP connection has failed!";
-	echo "Attempted to connect to $ftp_server for user $ftp_user_name";
-	exit;
-} else {
-	echo "Connected to $ftp_server, for user $ftp_user_name";
-}
-
-// get contents of the current directory
-$contents = ftp_nlist($conn_id, ".");
-debug($contents);
-
-/*
- * Get the file
- * ftp_pwd
- * ftp_nlist
- * ftp_fget
- */
-
 $temp_handle = fopen('php://temp', 'r+');
-ftp_fget( $conn_id, $temp_handle, "", FTP_ASCII );
-// close the FTP stream
-ftp_close($conn_id);
+ftp_fget( $conn_id, $temp_handle, "NPS_OSAX_STK.csv", FTP_ASCII );
+ftp_close( $conn_id );
+
+rewind($temp_handle);
 
 if ( !empty( $temp_handle ) ) {
-
-	// Alustukset
-	$ohita_otsikkorivi = !empty($_POST['otsikkorivi']);
-	$rows_in_query_at_one_time = 15000;
-	$values = array();
-
 	if ( $ohita_otsikkorivi ) {
-		fgetcsv($temp_handle, 100, ";");
+		fgetcsv($temp_handle, 1, ";");
 	}
 
-	while (($data = fgetcsv($temp_handle, 1000, ";")) !== false) {
+	$values = array();
+	while (($data = fgetcsv($temp_handle, 200, ";")) !== false) {
 		$values[] = (int)$hankintapaikka_id; // hkp-ID
 		$values[] = utf8_encode($data[0]);  // tuote artikkeli-nro
-		$values[] = (int)$data[1]; // tehdassaldo
+		$values[] = ($data[1] === "0") ? 0 : 1; // tehdassaldo
 	}
 
 	$sql = "INSERT INTO toimittaja_tehdassaldo (hankintapaikka_id, tuote_articleNo, tehdassaldo) VALUES (?,?,?)
 			ON DUPLICATE KEY UPDATE tehdassaldo = VALUES(tehdassaldo)";
 
-	$values = array_chunk($values,$rows_in_query_at_one_time);
+	$values = array_chunk( $values, $rows_in_query_at_one_time );
 
 	foreach ( $values as $values_chunk ) {
 		$db->query(
@@ -81,4 +51,3 @@ if ( !empty( $temp_handle ) ) {
 			$values_chunk);
 	}
 }
-
