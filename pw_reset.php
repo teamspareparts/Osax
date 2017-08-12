65ce0390f8e10820256499b0a3e3acf593277c8e
juhanj <juhani.jarvio@gmail.com>
2017-08-12 12:14:33 +0300

    Siistitty pw_reset sivua
    Ei tarkistanut ennen serveri puolella ss:n pituutta. Korjattu. Lisäksi
    käyttää nyt uutta error-boxia kirjautumissivulta.

--- pw_reset.php
+++ pw_reset.php
@@ -57,8 +57,7 @@ function hae_kayttaja ( DByhteys $db, stdClass $pw_reset ) {
  */
 function db_vaihda_salasana ( DByhteys $db, stdClass $user, /*string*/ $uusi_salasana, $reset_key ) {
 	$hajautettu_uusi_salasana = password_hash($uusi_salasana, PASSWORD_DEFAULT);
-	$sql = "UPDATE kayttaja SET salasana_hajautus = ?, salasana_vaihdettu=NOW(), salasana_uusittava = 0
-			WHERE id = ?";
+	$sql = "UPDATE kayttaja SET salasana_hajautus = ?, salasana_vaihdettu=NOW(), salasana_uusittava = 0 WHERE id = ?";
 	$db->query( $sql, [$hajautettu_uusi_salasana, $user->id] );
 
 	//Merkataan avain käytetyksi
@@ -67,9 +66,8 @@ function db_vaihda_salasana ( DByhteys $db, stdClass $user, /*string*/ $uusi_sal
 }
 
 if ( empty($_GET['id']) ) {
-	if ( empty($_SESSION['id']) ) {
-		header("location:etusivu.php"); exit();
-	} else header("location:index.php"); exit();
+	empty($_SESSION['id']) ? header("location:etusivu.php") : header("location:index.php");
+	exit();
 }
 
 $db = new DByhteys();
@@ -79,17 +77,23 @@ $pw_reset = tarkista_pw_reset_key_ja_aika( $db, $reset_id_hash ); // Tässä koh
 $reset_user = hae_kayttaja( $db, $pw_reset );
 
 if ( !empty($_POST['reset']) ) {
-	if ( $_POST['new_password'] === $_POST['confirm_new_password'] ) {
+	if ( $_POST['new_password'] !== $_POST['confirm_new_password'] ) {
+		$_SESSION['feedback'] = "<p class='error'>Salasana ja varmistus eivät täsmää.</p>";
+	}
+	elseif ( strlen($_POST['new_password']) < 8 ) {
+		$_SESSION['feedback'] = "<p class='error'>Salasanan pitää olla vähintään kahdeksan merkkiä pitkä.</p>";
+	}
+	else {
 		db_vaihda_salasana( $db, $reset_user, $_POST['new_password'], $reset_id_hash );
-		header("location:index.php?redir=8"); exit;
-	} else {
-		$error = TRUE;
+		header("location:index.php?redir=8");
+		exit;
 	}
 }
 
 /** Tarkistetaan feedback, ja estetään formin uudelleenlähetys */
 if ( !empty($_POST) ) { //Estetään formin uudelleenlähetyksen
-	header("Location: " . $_SERVER['REQUEST_URI']); exit();
+	header("Location: " . $_SERVER['REQUEST_URI']);
+	exit();
 } else {
 	$feedback = isset($_SESSION['feedback']) ? $_SESSION['feedback'] : "";
 	unset($_SESSION["feedback"]);
@@ -98,22 +102,11 @@ if ( !empty($_POST) ) { //Estetään formin uudelleenlähetyksen
 <!DOCTYPE html>
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="css/login_styles.css">
 	<meta charset="UTF-8">
 	<title>Password reset</title>
-    <script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
-	<style>
-		.info {
-			position: relative;
-			display: block;
-			padding: 10pt;
-			margin: 1em;
-			width: auto;
-			font-weight: bold;
-			background-color: lightsteelblue;
-		}
-	</style>
+	<link rel="stylesheet" type="text/css" href="css/login_styles.css">
+	<script src="https://code.jquery.com/jquery-3.1.1.min.js"></script>
 </head>
 <body>
 
@@ -122,23 +115,17 @@ if ( !empty($_POST) ) { //Estetään formin uudelleenlähetyksen
 
 	<?= $feedback ?>
 
-	<?php if ( $error ) : ?>
-		<fieldset id=error><legend> Salasana ja varmistus eivät täsmää </legend>
-			<p>Kokeile uudestaan. Varmista, että antamasi salasana ja varmistus täsmäävät.</p>
-		</fieldset>
-	<?php endif; ?>
-
 	<fieldset><legend> Salasanan vaihto </legend>
 		<form action="pw_reset.php?id=<?=$_GET['id']?>" method="post" accept-charset="utf-8">
 			<?= $reset_user->sahkoposti // Muistutuksena käyttajalle ?>
 			<br><br>
 			<label for="uusi_salasana">Uusi salasana</label>
-			<input type="password" name="new_password" id="uusi_salasana" pattern=".{6,}"
-				   title="Pituus min 6 merkkiä." placeholder="Uusi salasana" required autofocus>
+			<input type="password" name="new_password" id="uusi_salasana" pattern=".{8,}"
+				   title="Pituus min 8 merkkiä." placeholder="Uusi salasana" required autofocus>
 			<br><br>
 			<label for="vahv_uusi_salasana">Vahvista uusi salasana</label>
-			<input type="password" name="confirm_new_password" id="vahv_uusi_salasana" pattern=".{6,}"
-				   title="Pituus min 6 merkkiä." placeholder="vahvista uusi salasana" required autofocus>
+			<input type="password" name="confirm_new_password" id="vahv_uusi_salasana" pattern=".{8,}"
+				   title="Pituus min 8 merkkiä." placeholder="vahvista uusi salasana" required>
 			<span id="check"></span>
 			<br><br>
 			<input type="hidden" name="reset" value="password_reset">
