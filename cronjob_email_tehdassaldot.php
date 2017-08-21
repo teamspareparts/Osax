223d45528cb3990f23c8395ba886a4c201387cca
juhanj <juhani.jarvio@gmail.com>
2017-08-21 14:03:50 +0300

    Korjauksia tehdassaldojen lukemiseen emailista
    Pitäisi toimia nyt.

--- cronjob_email_tehdassaldot.php
+++ cronjob_email_tehdassaldot.php
@@ -16,37 +16,38 @@ while (false !== ($entry = $d->read())) {
 	$filepath = "{$tiedosto_polku}/{$entry}";
 	// could do also other checks than just checking whether the entry is a file
 	if (is_file($filepath) && filemtime($filepath) > $latest_emtime) {
+		$latest_emtime = filemtime($filepath);
 		$latest_filepath = $filepath;
 	}
 }
 
 $parser = new EmailParser( file_get_contents($latest_filepath) );
 
-$b = array_map(
+$csv_array = array_map(
 	function($val) { return explode(';', $val); },
 	explode("\n", str_replace(array("\r\n","\n\r","\r"),"\n", $parser->getAttachments()[0]['body'])) );
 
-array_shift( $b ); //TODO: slow for big arrays, find better way.
+array_shift( $csv_array ); //TODO: slow for big arrays, find better way.
 
+if ( !empty( $csv_array ) ) {
 
-if ( !empty( $b ) ) {
-
-	foreach ( $b as $t ) {
-		$t[0] = utf8_encode( str_replace( [" ","'"], "", $t[ 0 ] ) );  // tuote artikkeli-nro
-		$t[1] = (int)$t[ 1 ]; // tehdassaldo
-		$t[] = $hankintapaikka_id;
+	foreach ( $csv_array as &$row ) {
+		$row[0] = utf8_encode( str_replace( [" ","'"], "", $row[ 0 ] ) );  // tuote artikkeli-nro
+		$row[1] = (int)$row[ 1 ]; // tehdassaldo
+		$row[] = $hankintapaikka_id;
 	}
 
-	$sql = "INSERT INTO toimittaja_tehdassaldo (hankintapaikka_id, tuote_articleNo, tehdassaldo) VALUES (?,?,?)
+	$sql = "INSERT INTO toimittaja_tehdassaldo (tuote_articleNo, tehdassaldo, hankintapaikka_id) VALUES (?,?,?)
 			ON DUPLICATE KEY UPDATE tehdassaldo = VALUES(tehdassaldo)";
 
-	$values = array_chunk( $values, $rows_in_query_at_one_time );
+	$values = array_chunk( $csv_array, $rows_in_query_at_one_time );
 
 	foreach ( $values as $values_chunk ) {
 		$db->query(
 			str_replace("(?,?,?)",
-						str_repeat('(?, ?, ?),', (count($values_chunk)/3)-1) . "(?, ?, ?)",
+						str_repeat('(?, ?, ?),', (count($values_chunk)-1)) . "(?, ?, ?)",
 						$sql),
-			$values_chunk);
+			iterator_to_array(new RecursiveIteratorIterator(new RecursiveArrayIterator($values_chunk)),false)
+		);
 	}
 }
