9ec9e61ae3119a8be5a027273170fa5c55a0f4bd
samileh <smai_lehtinen@hotmail.com>
2018-01-14 20:20:35 +0200

    Webservice funktiota
    Tarpeellisia funktioita, joita tarvitaan webservicestä tilaamiseen.

--- luokat/eoltaswebservice.class.php
+++ luokat/eoltaswebservice.class.php
@@ -71,7 +71,7 @@ class EoltasWebservice {
 	}
 
 	/**
-	 * Hakee ostoskorin.
+	 * Hakee ostoskorin sisällön.
 	 * @return stdClass
 	 */
 	static function getBasket() : stdClass {
@@ -128,24 +128,61 @@ class EoltasWebservice {
 	 * Palauttaa config tiedostoon tallennetun Eoltaksen hankintapaikka id:n.
 	 * @return int
 	 */
-	static function getEoltasHankintapaikkaId() : int {
+	private static function getEoltasHankintapaikkaId() : int {
 		$config = parse_ini_file( self::$config_path );
 		return (int)$config['eoltas_hankintapaikka_id'];
+	}
+
+	/**
+	 * Tarkastetaan onko tuotteen hankintapaikka Eoltas.
+	 * @param int $id
+	 * @return bool
+	 */
+	private static function checkEoltasHankintapaikkaId( int $id ) : bool {
+		$eoltas_hankintapaikka_id = EoltasWebservice::getEoltasHankintapaikkaId();
+		if ( $id != $eoltas_hankintapaikka_id) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Hakee Eoltaksen tuote-id:n.
+	 * @param string $articleNo
+	 * @param string $brandName
+	 * @param string $brandId
+	 * @return string|null
+	 */
+	private static function getEoltasProductId( string $articleNo, string $brandName, string $brandId ) {
+		$eoltas_data = EoltasWebservice::searchProduct( $articleNo , $brandName );
+		// Tarkistetaan webservice-yhteys
+		if ( !$eoltas_data || !$eoltas_data->acknowledge ) {
+			trigger_error('Cannot connect to Eoltas webservice.');
+		}
+		// Etsitään oikea tuote
+		foreach ( $eoltas_data->response->products as $product) {
+			$product->supplierCode = str_replace(" ", "", $product->supplierCode);
+			$articleNo = str_replace(" ", "", $articleNo);
+			if ( strcasecmp($articleNo, $product->supplierCode) === 0 && $brandId == $product->brandId ) {
+				// Oikea tuote löytyi
+				return $product->id;
+			}
+		}
+		return null;
 	}
 
 	/**
 	 * Hakee ja palauttaa Eoltas tuotteen tehdassaldon.
 	 * @param int $hankintapaikka_id
 	 * @param string $articleNo
+	 * @param string $brandName
 	 * @return int|null
 	 */
 	static function getEoltasTehdassaldo( int $hankintapaikka_id, string $articleNo, string $brandName ) {
-		$eoltas_hankintapaikka_id = EoltasWebservice::getEoltasHankintapaikkaId();
-		// Tehdään webservice haku vain Eoltaksen tuotteille
-		if ( $hankintapaikka_id != $eoltas_hankintapaikka_id) {
+		if ( !self::checkEoltasHankintapaikkaId( $hankintapaikka_id ) ) {
 			return null;
 		}
-		$eoltas_data = EoltasWebservice::searchProduct( $articleNo , $brandName);
+		$eoltas_data = EoltasWebservice::searchProduct( $articleNo , $brandName );
 		// Tarkistetaan webservice-yhteys
 		if ( !$eoltas_data || !$eoltas_data->acknowledge ) {
 			trigger_error('Cannot connect to Eoltas webservice.');
@@ -161,10 +198,66 @@ class EoltasWebservice {
 		return null;
 	}
 
-
-	private static function addProductToEoltasOrderBook( string $articleNo, string $brandNo ) : bool {
+	/**
+	 * Lisää tuotteen Eoltaksen avoimelle ostotilauskirjalle.
+	 * @param DByhteys $db
+	 * @param string $articleNo
+	 * @param string $brandNo
+	 * @return bool
+	 */
+	private static function addProductToEoltasOrderBook( DByhteys $db, int $id, int $kpl ) : bool {
 		$eoltas_hankintapaikka_id = EoltasWebservice::getEoltasHankintapaikkaId();
+		// Haetaan Eoltaksen avoimen ostotilauskirjan id
+		$sql = "SELECT id FROM ostotilauskirja WHERE hankintapaikka_id = ? AND toimitusjakso != 0";
+		$otk_id = $db->query($sql, [$eoltas_hankintapaikka_id]);
+		if ( !$otk_id ) {
+			return false;
+		}
+		// Lisätään tuote ostotilauskirjalle
+		//TODO: KESKEN!! Selite valmiiksi!!
+		$sql = "INSERT INTO ostotilauskirja_tuote (ostotilauskirja_id, tuote_id, automaatti,
+ 					kpl, selite, lisays_kayttaja_id)
+				VALUES (?,?,0,?,?,0)
+				ON DUPLICATE KEY UPDATE kpl = kpl + VALUES(kpl)";
+		$res = $db->query($sql, [$otk_id, $id, $kpl]);
+		return false;
+	}
 
+	//TODO: KESKEN
+	static function addProductToBasket( DByhteys $db, int $tuote_id, int $kpl ) : bool {
+		$sql = "SELECT hankintapaikka_id, articleNo, brandNo, valmistaja FROM tuote WHERE id = ?";
+		$tuote = $db->query($sql, [$tuote_id]);
+		if ( !$tuote ) {
+			return false;
+		}
+		// Webservice-kyselyt vain Eoltaksen tuotteille
+		if ( !self::checkEoltasHankintapaikkaId( $tuote->hankintapaikka_id ) ) {
+			return false;
+		}
+		// Haetaan Eoltaksen oma tuote-id
+		$eoltas_id = self::getEoltasProductId( $tuote->articleNo, $tuote->brandNo, $tuote->bradName );
+		if ( !$eoltas_id ) {
+			return false;
+		}
+		// Lisätään tuote ostoskoriin
+		//$ostoskori = self::addToBasket( $eoltas_id, $kpl );
+		//self::addProductToEoltasOrderBook( $db, $tuote->id, $kpl );
+		return false;
+	}
+
+	/**
+	 * Tarkastetaan, että tilattavia tuotteita on tarpeeksi.
+	 * @return bool
+	 */
+	static function checkBasketValidity() : bool {
+		$ostoskori = self::getBasket();
+		foreach ( $ostoskori->response->basket as $product ) {
+			if ( $product->amount > $product->stock ) {
+				// Tilattavia tuotteita enemmän kuin varastossa
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
