c83c8d91805c0ac49ab2cfeba3ac7e47a33de11c
juhanj <juhani.jarvio@gmail.com>
2018-01-15 17:17:19 +0200

    Pohja tuoteMyyntitiedot luokalle

---
+++ luokat/tuotemyyntitiedot.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jjarv
+ * Date: 15/01/2018
+ * Time: 15:50
+ */
+
+class tuoteMyyntitiedot {
+
+	public static function haeVastaavat( int $id ) {
+
+		$sql = "";
+
+		$rows = $db->query( $sql, [ ], FETCH_ALL );
+	}
+
+}
