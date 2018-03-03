4be6d9b9a97225d1153b7f192412fae0cc781276
juhanj <juhani.jarvio@gmail.com>
2018-03-03 11:25:28 +0200

    Lasku-luokka
    PHP7, strict_types. Extends Tilaus, lisätty luoLaskuNumero, ja
    maksutapa_toString. Jälkimmäinen overrides parent method.

--- luokat/lasku.class.php
+++ luokat/lasku.class.php
@@ -1,9 +1,52 @@
-<?php
+<?php declare(strict_types=1);
+
 /**
- * @version 2017-02-xx <p> WIP
+ * Class Lasku extends Tilaus
  */
 class Lasku extends Tilaus {
 
-	//TODO: WIP
+	public $laskuHeader = null;
 
+	/** @var Yritys */
+	public $osax = null;
+
+
+	/**
+	 * @param DByhteys $db
+	 * @param int      $tilaus_id
+	 * @param int      $indev
+	 */
+	function __construct( DByhteys $db, int $tilaus_id = null, $indev = 1 ) {
+
+		parent::__construct( $db, $tilaus_id );
+
+		$this->osax = new Yritys( $db, 1 );
+
+		$this->laskuHeader = ( $indev )
+			? "<h2 style='color: red;'>InDev testilasku</h2>"
+			: "<img src='../img/osax_logo.jpg' alt='Osax.fi'>";
+
+	}
+
+	function luoLaskunNumero( DByhteys $db ) {
+		if ( $this->laskunro === null ) {
+			$sql = "SELECT laskunro FROM laskunumero LIMIT 1";
+			$row = $this->db->query( $sql );
+			$this->laskunro = $row->laskunro;
+			$sql = "UPDATE laskunumero SET laskunro = laskunro + 1 LIMIT 1";
+			$this->db->query( $sql );
+			$sql = "UPDATE tilaus SET laskunro = ? WHERE id = ? LIMIT 1";
+			$this->db->query( $sql, [ $this->laskunro, $this->tilaus_nro ] );
+		}
+	}
+
+	/**
+	 * @param bool $alku <p> Onko tulostus laskun alkuun vai loppuun? Lopussa pidempi teksti.
+	 * @return string
+	 */
+	function maksutapa_toString ( bool $alku = true ) {
+		return ($this->maksutapa)
+			? ( $alku ? "Lasku 14 pv." : "! Maksetaan laskulla &mdash; maksuaika 14 päivää !" )
+			: ( $alku ? "e-korttimaksu" : "! Maksettu korttiveloituksena tilausta tehdessä !" );
+	}
 }
